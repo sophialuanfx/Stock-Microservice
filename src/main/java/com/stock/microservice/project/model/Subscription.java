ec44d9e3a788f679727f3e81d24dbe7d78768b3c
Arthurwwr <wuwenrui2002@gmail.com>
2024-12-01 19:57:31 -0500

    Update Subscription.java

--- src/main/java/com/stock/microservice/project/model/Subscription.java
+++ src/main/java/com/stock/microservice/project/model/Subscription.java
@@ -1,1 +1,42 @@
 
+package com.stock.microservice.project.model;
+
+public class Subscription {
+    private Long id;
+    private Long userID;
+    private String stockSymbol;
+    private String notificationType;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    public void setStockSymbol(String stockSymbol) {
+        this.stockSymbol = stockSymbol;
+    }
+
+    public String getNotificationType() {
+        return notificationType;
+    }
+
+    public void setNotificationType(String notificationType) {
+        this.notificationType = notificationType;
+    }
+}
